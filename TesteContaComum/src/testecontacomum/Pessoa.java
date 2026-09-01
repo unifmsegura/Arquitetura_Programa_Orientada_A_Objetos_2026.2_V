7c0c4d8a3422ad417e3db478f5296f66e85159d6
unifmsegura <matheusbesegura@gmail.com>
2026-09-01 13:14:18 -0300

    atua

--- TesteContaComum/src/testecontacomum/Pessoa.java
+++ TesteContaComum/src/testecontacomum/Pessoa.java
@@ -8,41 +8,34 @@ package testecontacomum;
  *
  * @author unifmsegura
  */
-public class Pessoa {
-    protected String nome, endereco;
-    protected double renda;
-    protected Data dataNascimento; // agregação, associação
+class Pessoa { 
+    protected String nome, endereco; 
+    protected double renda; 
+    protected Data dataNascimento; 
     protected ContaComum conta;
-    
-    public Pessoa(String nome, String endereco, double renda, Data dataNasc){
-        this.nome = nome;
-        this.endereco = endereco;
-        this.renda = renda;
-        this.dataNascimento = dataNasc;
-        this.conta = new ContaComum();
+
+    public Pessoa(String nome, String endereco, double renda, Data dataNasc){ 
+        this.nome = nome; 
+        this.endereco = endereco; 
+        this.renda = renda; 
+        this.dataNascimento = dataNasc; 
+        this.conta = new ContaComum(); 
     }
-    
-    public Pessoa(String nome, String endereco, double renda, Data dataNasc, ContaComum conta){
-        this.nome = nome;
-        this.endereco = endereco;
-        this.renda = renda;
-        this.dataNascimento = dataNasc;
-        this.conta = conta;
+
+    public Pessoa(String nome, String endereco, double renda, Data dataNasc, ContaComum conta){ 
+        this.nome = nome; 
+        this.endereco = endereco; 
+        this.renda = renda; 
+        this.dataNascimento = dataNasc; 
+        this.conta = conta; 
     }
-    
-    
-    
-    public void printPessoa(){
-        System.out.printf("Nome=%s, End=%s, Renda=%.2f | Nasc=", nome, endereco, renda);
-        this.dataNascimento.printData(); // bom uso da agregação, dataNascimento é objeto dentro da classe Data
-        this.conta.printConta();
+
+    public void printPessoa(){ 
+        System.out.printf("Nome=%s, End=%s, Renda=%.2f | Nasc=", nome, endereco, renda); 
+        this.dataNascimento.printData(); 
+        this.conta.printConta(); 
     }
-    
-    public Data getDataNascimento(){
-        return dataNascimento;
-    }
-    
-    public ContaComum getConta(){
-        return conta;
-    }
+
+    public Data getDataNascimento(){ return dataNascimento; }
+    public ContaComum getConta(){ return conta; } 
 }
