5af0f7897faab84976b1e165930b802d8b8e4917
unifmsegura <matheusbesegura@gmail.com>
2026-09-01 13:15:37 -0300

    atua
    Refactor main class to TesteContaComumCorrigido and fix output formatting issues.

--- TesteContaComum/src/testecontacomum/main.java
+++ TesteContaComum/src/testecontacomum/main.java
@@ -8,36 +8,31 @@ package testecontacomum;
  *
  * @author unifmsegura
  */
-public class main {
-
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Data data01 = new Data();
+public class TesteContaComumCorrigido {
+    public static void main(String[] args) { 
+        Data data01 = new Data(); 
         Pessoa pessoa01 = new Pessoa("Fulano", "SBC", 1000, data01);
-        
         Pessoa pessoa02 = new Pessoa("Beltrano", "SP", 2000, new Data(27, 8, 2006));
+        Pessoa pessoa03 = new Pessoa("Ciclano", "SA", 15000, new Data(1, 4, 1996), new ContaComum(20, 123456, new Data(27, 8, 2026), 20000));
         
-        Pessoa pessoa03 = new Pessoa("Ciclano", "SA", 15000, new Data(1, 4, 1996),
-                          new ContaComum(20, 123456, new Data(27, 8, 2026), 20000));
-        
-        pessoa01.printPessoa();
-        pessoa02.printPessoa();
+        System.out.println("--- Exibindo informações cadastradas ---");
+        pessoa01.printPessoa(); 
+        pessoa02.printPessoa(); 
         pessoa03.printPessoa();
+        System.out.println("----------------------------------------\n");
         
-        ContaComum conta02 = pessoa02.getConta();
-        System.out.printf("SaldoPessoa02=%.2f", conta02.getSaldo());
+        ContaComum conta02 = pessoa02.getConta(); 
+        System.out.printf("SaldoPessoa02=%.2f\n", conta02.getSaldo()); // CORRIGIDO: Adicionado \n para quebra de linha
         
-        Data dataAbert = conta02.getDataAbertura();
-        System.out.println("Data Abertura Conta pessoa02: ");
+        Data dataAbert = conta02.getDataAbertura(); 
+        System.out.println("Data Abertura Conta pessoa02: "); 
         dataAbert.printData();
         
-        int mesPessoa03 = pessoa03.getConta().getDataAbertura().getMes();
-        System.out.println("Mes da abertura da conta da pessoa03: %d", mesPessoa3);
+        // CORRIGIDO 1: Mudança de System.out.println para printf para aceitar formatação %d
+        // CORRIGIDO 2: Correção do nome da variável de 'mesPessoa3' para 'mesPessoa03' (com 0) para evitar erro de compilação
+        int mesPessoa03 = pessoa03.getConta().getDataAbertura().getMes(); 
+        System.out.printf("Mes da abertura da conta da pessoa03: %d\n", mesPessoa03); 
         
-        System.out.printf("Saldo da Pessoa03=%.2f", pessoa03.getConta().getSaldo());
+        System.out.printf("Saldo da Pessoa03=%.2f\n", pessoa03.getConta().getSaldo()); // CORRIGIDO: Adicionado \n para quebra de linha
     }
-    
 }
