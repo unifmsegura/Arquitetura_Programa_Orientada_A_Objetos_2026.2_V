61cfe2f15dbb634debb69343dfc88fdffeacc3a6
unifmsegura <matheusbesegura@gmail.com>
2026-09-17 12:07:18 -0300

    Update MovableCircle.java

--- A06E04/src/main/java/com/mycompany/a06e04/MovableCircle.java
+++ A06E04/src/main/java/com/mycompany/a06e04/MovableCircle.java
@@ -8,35 +8,36 @@ package com.mycompany.a06e04;
  *
  * @author unifmsegura
  */
-public class MovableCircle {
+class MovableCircle implements Movable {
     private int radius;
-    private MovablePoint center;
-    
-    public MovableCircle (int x, int y, int xSpeed, int ySpeed, int radius) {
+    private MovablePoint center; // Composição (1 MovablePoint)
+
+    // Construtor parametrizado
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
         this.radius = radius;
         this.center = new MovablePoint(x, y, xSpeed, ySpeed);
     }
-    
+
     @Override
-    public String toString () {
-        return String.format("Calculo no centro %s, raio=%d", center.toString(), radius);
+    public String toString() {
+        return String.format("Círculo no centro %s, raio=%d", center.toString(), radius);
     }
-    
+
     @Override
     public void moveUp() {
         center.moveUp();
     }
-    
+
     @Override
     public void moveDown() {
         center.moveDown();
     }
-    
+
     @Override
     public void moveLeft() {
         center.moveLeft();
     }
-    
+
     @Override
     public void moveRight() {
         center.moveRight();
