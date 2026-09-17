84ab48924a8f302b7a00e10ab4b0eeb89979dd4e
unifmsegura <matheusbesegura@gmail.com>
2026-09-17 12:09:14 -0300

    Update Area.java

--- A06E03/src/main/java/com/mycompany/a06e03/Area.java
+++ A06E03/src/main/java/com/mycompany/a06e03/Area.java
@@ -8,15 +8,27 @@ package com.mycompany.a06e03;
  *
  * @author unifmsegura
  */
-public class Area {
+class Area {
+
+    /**
+     * Calcula e imprime a área de um quadrado (1 parâmetro).
+     * 
+     * @param lado O lado do quadrado
+     */
     public void calcularArea(double lado) {
         double area = lado * lado;
-        System.out.printf("Area do Quadrado (lado + %.2f): %.2f\n", lado, area);
+        System.out.printf("Área do Quadrado (lado = %.2f): %.2f\n", lado, area);
     }
-    
+
+    /**
+     * Calcula e imprime a área de um retângulo (2 parâmetros: comprimento e largura).
+     * 
+     * @param comprimento O comprimento do retângulo
+     * @param largura A largura do retângulo
+     */
     public void calcularArea(double comprimento, double largura) {
         double area = comprimento * largura;
-        System.out.printf("Area do Retangulo (comprimento = %.2f, largura = %.2f): %.2f\n",
+        System.out.printf("Área do Retângulo (comprimento = %.2f, largura = %.2f): %.2f\n", 
                 comprimento, largura, area);
     }
 }
