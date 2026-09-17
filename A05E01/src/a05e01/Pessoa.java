f62eeb56a3ea96e8461e10d814a21c405d982cb2
unifmsegura <matheusbesegura@gmail.com>
2026-09-17 12:32:30 -0300

    Update Pessoa.java

--- A05E01/src/a05e01/Pessoa.java
+++ A05E01/src/a05e01/Pessoa.java
@@ -3,21 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package a05e01;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author unifmsegura
  */
-public class Pessoa {
-    protected String nome;
-    protected String sobrenome;
-    protected int idade; 
 
+/**
+ * Classe Pessoa - Superclasse da hierarquia de herança.
+ * Representa uma pessoa genérica com nome, sobrenome e idade.
+ */
+class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+    /**
+     * Construtor padrão (sem argumentos).
+     */
+    public Pessoa() {
+        this.nome = "Não informado";
+        this.sobrenome = "Não informado";
+        this.idade = 0;
+    }
+
+    /**
+     * Construtor parametrizado da classe Pessoa.
+     * 
+     * @param nome Nome da pessoa
+     * @param sobrenome Sobrenome da pessoa
+     * @param idade Idade da pessoa
+     */
     public Pessoa(String nome, String sobrenome, int idade) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.idade = idade;
     }
+
+    // --- Métodos Getters e Setters ---
 
     public String getNome() {
         return nome;
@@ -27,11 +52,11 @@ public class Pessoa {
         this.nome = nome;
     }
 
-    public String getSobrenome() {
+    public String getSobreNome() {
         return sobrenome;
     }
 
-    public void setSobrenome(String sobrenome) {
+    public void setSobreNome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
 
@@ -42,6 +67,5 @@ public class Pessoa {
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    
 }
 
