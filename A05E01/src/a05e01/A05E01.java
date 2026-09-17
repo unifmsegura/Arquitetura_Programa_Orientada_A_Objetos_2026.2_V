562973ebdac9f57dd5c1fa97e1cfb78e8bd0d4f0
unifmsegura <matheusbesegura@gmail.com>
2026-09-17 12:33:49 -0300

    Update A05E01.java

--- A05E01/src/a05e01/A05E01.java
+++ A05E01/src/a05e01/A05E01.java
@@ -17,38 +17,48 @@ public class A05E01 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         ArrayList<Aluno> listaAlunos = new ArrayList<>();
-        
-        System.out.println("Entre com os dados do aluno:");
+
+        System.out.println("=================================================");
+        System.out.println("       EXERCÍCIO 1 - HIERARQUIA DE ALUNO        ");
+        System.out.println("=================================================\n");
+
+        // Entrada de dados via teclado
+        System.out.println("Entre com os dados do aluno para o construtor parametrizado:");
         
         System.out.print("Digite o nome: ");
         String nome = leitor.nextLine().trim();
-        
+
         System.out.print("Digite o sobrenome: ");
         String sobrenome = leitor.nextLine().trim();
-       
+
         System.out.print("Digite a idade: ");
         int idade = leitor.nextInt();
-        leitor.nextLine();
-        
+        leitor.nextLine(); // Limpa o buffer do teclado
+
         System.out.print("Digite o curso: ");
         String curso = leitor.nextLine().trim();
         System.out.println();
-        
-        Aluno aluno1 = new Aluno("Ciencia da Computacao", "Gabriel", "Obina", 20);
-        Aluno aluno2 = new Aluno("Ciencia da Computacao", "Gustavo", "Couto", 20);
-        Aluno alunoNovo = new Aluno(curso, nome, sobrenome, idade);
-        
+
+        // 1. Instanciação de objeto sem passar argumentos (construtor padrão)
+        Aluno aluno1 = new Aluno();
+
+        // 2. Instanciação de objeto com o construtor parametrizado
+        Aluno aluno2 = new Aluno(nome, sobrenome, idade, curso);
+
+        // Adiciona ambos no ArrayList
         listaAlunos.add(aluno1);
         listaAlunos.add(aluno2);
-        listaAlunos.add(alunoNovo);
-        
-        System.out.println("ArrayList dos Alunos:");
+
+        // Exibição do conteúdo do ArrayList utilizando o método print()
+        System.out.println("=================================================");
+        System.out.println("          CONTEÚDO DO ARRAYLIST DE ALUNOS        ");
+        System.out.println("=================================================");
         
         for (int i = 0; i < listaAlunos.size(); i++) {
             System.out.println("Aluno " + (i + 1) + ":");
             listaAlunos.get(i).print();
         }
-        
+
         leitor.close();
     }
 }
